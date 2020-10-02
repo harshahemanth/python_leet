f4ba69652c071a7979bafe7a6521280b8f4ef65d
harshahemanth <chundurihemanth1997@gmail.com>
2020-10-02 17:43:39 +0530

    Practice arrays
    Arrays Practice

---
+++ arrays.py
@@ -0,0 +1,17 @@
+# 1. Given a binary array, find the maximum number of consecutive 1s in this array.
+# Input: [1,1,0,1,1,1]
+# Output: 3
+def get_max_length(nums):
+    length_of_arr = len(nums)
+    count = 0
+    maximum_count = 0
+    for i in range(0, n):
+        if length_of_arr[i] == 0:
+            maximum_count = 0
+        else:
+            count += 1
+            maximum_count = max(maximum_count, count)
+    return maxximum_count
+
+# Test Case: nums = [1, 0, 1, 1, 0, 1]
+#print(get_max_length(nums))
