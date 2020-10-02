cfe15928c5bc30642d6449522cea847fee6cc194
harshahemanth <chundurihemanth1997@gmail.com>
2020-10-02 18:23:17 +0530

    even number of digits in array

--- arrays.py
+++ arrays.py
@@ -5,7 +5,7 @@ def get_max_length(nums):
     length_of_arr = len(nums)
     count = 0
     maximum_count = 0
-    for i in range(0, n):
+    for i in range(0, nums):
         if length_of_arr[i] == 0:
             maximum_count = 0
         else:
@@ -13,5 +13,19 @@ def get_max_length(nums):
             maximum_count = max(maximum_count, count)
     return maximum_count
 
-# Test Case: nums = [1, 0, 1, 1, 0, 1]
+
 # print(get_max_length(nums))
+
+# -------------------------------------------------------------------------------------------#
+
+# 2. Given an array nums of integers, return how many of them contain an even number of digits.
+# Input: nums = [12,345,2,6,7896]
+# Output: 2
+def find_numbers(nums):
+    count_of_even = 0
+    for number in nums:
+        num_to_string = str(number)
+        if len(num_to_string) % 2 == 0:
+            count_of_even += 1
+    return count_of_even
+
